86312ff6061f35a6e05c729be598da6d46cb66bb
Charles Williams <charlesjw94@outlook.com>
2015-09-23 23:43:44 -0400

    Added a library for sound and animation.

--- animation.js
+++ animation.js
@@ -2,20 +2,42 @@
 //I drew two seperate pictures of a flame burning.
 //I do not believe that my code is correct for this one either.
 //I created an Animation folder that has the two pictures that will be used for the background animation
-function Animation( ) {
+
 
 var fireAvatar;
 
+function construct(constructor, args) {
+    'use strict';
+    function F() {
+        return constructor.apply(this, args);
+    }
+    F.prototype = constructor.prototype;
+    return new F();
+}
+
+p5.prototype.loadAnimation = function () {
+    'use strict';
+    return construct(Animation, arguments);
+};
+
 function preload() {
+    'use strict';
+//create an animation from a sequence of numbered images
+//pass the first and the last file name and it will try to find the ones in between
+    fireAvatar = loadAnimation("animation/flame1.png", "animation/flame2.png");
+}
 
-fireAvatar = loadAnimation("animation/flame1.png", "animation/flame2.png");
-
+function setup() {
+    'use strict';
+    createCanvas(800, 300);
 }
 
 function draw() {
+    'use strict';
+    background(255, 255, 255);
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(fireAvatar, 300, 150);
+    animation(fireAvatar, 300, 150);
 }
-}
+
--- sounds.js
+++ sounds.js
@@ -1,16 +1,23 @@
-//This function was suppose to be for the sound effect as the rope burned, but I could not seemt to get it right
-//I created a folder that has the sound file, I will look into this some more so I can get it working, hopefully
+var mySound;
+p5.prototype.registerPreloadMethod('loadSound', p5);
 
-function Sound() {
+function loadSound(path, callback, whileLoading) {
+    'use strict';
+    // if loading locally without a server
+    if (window.location.origin.indexOf('file://') > -1 && window.cordova === 'undefined') {
+        alert('This sketch may require a server to load external files. Please see http://bit.ly/1qcInwS');
+    }
+    var s = new p5.SoundFile(path, callback, whileLoading);
+    return s;
+}
 
 function preload() {
-    "use strict";
+    'use strict';
     mySound = loadSound('Sound/Fire.mp3');
 }
 
 function setup() {
-    "use strict";
+    'use strict';
     mySound.setVolume(0.1);
     mySound.play();
-}
 }
