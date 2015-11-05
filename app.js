d268302421d39254371d474bf723fca158e30af4
James Morrow <jmorrow1@unca.edu>
2015-11-05 16:29:58 -0700

    Implemented Problem Generation
    To get a problem, call getRandomProblem([numTerms]). numTerms is an
    optional parameter. It's the number of terms the problem will have.
    There can be 1, 2, or 3 terms.

--- app.js
+++ app.js
@@ -5,19 +5,34 @@ var init = function (p5) {
     var spark_dx; //change in spark location per frame
     var cenNum = 0; // Number at center of number line
     
+    //problem variables
+    var problemText, answer;
+    
 	//p5.setup runs once before p5.draw is run
 	p5.setup  = function () {
 		p5.createCanvas(600, 600);
 		numberLine = new NumberLine(0, p5.height/2, p5.width, 40, cenNum, p5);
-		problemGenerator(cenNum, numberLine.getIntervalBound, p5);
+		//problemGenerator(cenNum, numberLine.getIntervalBound, p5);
 		rope = new Line(0, 300, numberLine.length(), 300, p5);
 		spark = new Point(0, 300, p5);
 		spark_dx = 1;
+        
+        var testProblem = getRandomProblem();
+        problemText = testProblem.getProblem();
+        answer = testProblem.getAnswer();
+        
 	};
 
     //p5.draw runs every frame by default
 	p5.draw = function () {
 		p5.background(255);
+        
+        p5.fill(0);
+        p5.textSize(32);
+        p5.textAlign(p5.CENTER, p5.TOP);
+        p5.text(problemText, p5.width/2, p5.height/3);
+        p5.textSize(12);
+        
         //draw number line:
         numberLine.draw();
 
@@ -27,7 +42,6 @@ var init = function (p5) {
             p5.strokeWeight(2);
             p5.stroke(255, 160, 0);
             rope.draw();
-            p5.println(rope.length());
 
             p5.strokeWeight(4);
             p5.stroke(100, 0, 0);
--- problemGenerator.js
+++ problemGenerator.js
@@ -1,12 +1,73 @@
-// Create problem that follows a + b = c, where a & b are given
-// integers and c must be solved by student playing game.
-// Answer is only generated within bounds of numberLine
-function problemGenerator (cenNum, bounds, p5) {
-	var a, b, c;
-	var l = cenNum - bounds; // lower bound of numberLine
-	var u = cenNum + bounds; // upper bound of numberLine
+var problemTemplates = [
+    //1-arity functions
+    [
+        {
+            arity: 1,
+            solver: function(args) {return args[0]},
+            problem: function(args) {return '' + args[0];}
+        }
+    ],
+    //2-arity functions
+    [
+        {
+            arity: 2,
+            solver: function(args) {return args[0] + args[1];},
+            problem: function(args) {return args[0] + ' + ' + args[1];}
+        },
+        {
+            arity: 2,
+            solver: function(args) {return args[0] - args[1];},
+            problem: function(args) {return args[0] + ' - ' + args[1];}
+        }
+    ],
+    //3-arity functions
+    [
+        {
+            arity: 3,
+            solver: function(args) {return args[0] + args[1] + args[2];},
+            problem: function(args) {return args[0] + ' + ' + args[1] + ' + ' + args[2];}
+        },
+        {
+            arity: 3,
+            solver: function(args) {return args[0] + args[1] - args[2];},
+            problem: function(args) {return args[0] + ' + ' + args[1] + ' - ' + args[2];}
+        },
+        {
+            arity: 3,
+            solver: function(args) {return args[0] - args[1] + args[2];},
+            problem: function(args) {return args[0] + ' - ' + args[1] + ' + ' + args[2];}
+        },
+        {
+            arity: 3,
+            solver: function(args) {return args[0] - args[1] - args[2];},
+            problem: function(args) {return args[0] + ' - ' + args[1] + ' - ' + args[2];}
+        },
+    ]
+];
 
-	c = p5.random(l, u);
-	a = p5.random(l - bounds, u + bounds);
-	b = (a - c) * -1;
+function getRandomProblem(numTerms) {
+    if (numTerms == undefined) {
+        numTerms = Math.floor(Math.random() * 3) + 1;
+    }
+    if (1 <= numTerms && numTerms <= 3) {
+        var args = [];
+        for (var i=0; i<numTerms; i++) {
+            args[i] = Math.floor((Math.random() * 10) + 1);
+        }
+        var index = Math.floor((Math.random() * problemTemplates[numTerms-1].length));
+        return new Problem(problemTemplates[numTerms-1][index], args);
+    }
+    else {
+        console.log("ERROR --- There are no " + numTerms + " functions.");
+    }
+}
+
+function Problem(template, args) {
+    this.getProblem = function() {
+        return template.problem(args);
+    }
+    
+    this.getAnswer = function() {
+        return template.solver(args);
+    }
 }
